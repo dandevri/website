d49f86c2e1843ef8afbdd5408269afe897eb7df7
dandevri <hi@dandevri.es>
2019-07-22 21:27:58 +0200

    Add placeholder pages

---
+++ _data/metadata.js
@@ -0,0 +1,13 @@
+const metadata = {
+  title: "Static Site Starter",
+  author: "Danny de Vries",
+  mail: "hi@dandevri.es",
+  url: "https://example.com",
+  description: "A boilerplate for building static websites",
+  theme: "#222",
+  lang: "en_US",
+  share: "/static/img/social/",
+  social: "dandevri"
+}
+
+module.exports = metadata;
---
+++ _data/uses.js
@@ -0,0 +1,25 @@
+const workstation = [
+  {
+    title: "Macbook 13 inch (Late 2017)",
+    link: "www.apple.com",
+    description: "This is my main machine and what I use for 99% of my work.",
+  },
+  {
+    title: "Logitech MX Master",
+    link: "https://www.apple.com/nl/",
+    description: "Mouse that I use to point",
+  },
+]
+
+const software = [
+  {
+    title: "VScode",
+    link: "www.apple.com",
+    description: "Code editor of choice",
+  },
+]
+
+module.exports = {
+  workstation,
+  software
+}
