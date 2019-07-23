904f4e7bab9a5506cf06c5affb0b55ae14e0e1fa
dandevri <hi@dandevri.es>
2019-07-23 11:46:40 +0200

    Add press kit

---
+++ _data/events.js
@@ -0,0 +1,14 @@
+const events2018 = [
+  {
+    title: "Golden Dot Awards 2019",
+    link: "https://www.cmd-amsterdam.nl/gda-2019/",
+    date: "2019-07-03",
+    location: "Amsterdam, The Netherlands",
+    video: "youtube.com",
+    slides: "slides.com"
+  },
+]
+
+module.exports = {
+  events2018
+}
