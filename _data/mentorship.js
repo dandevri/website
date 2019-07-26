14f23bdcebf42dead2ad04ab9928112755cd81dc
dandevri <hi@dandevri.es>
2019-07-26 14:50:45 +0200

    Update coaching page

---
+++ _data/mentorship.js
@@ -0,0 +1,53 @@
+const help = [
+  {
+    description: "Landing a job or internship",
+  },
+  {
+    description: "Portfolio and design critique",
+  },
+  {
+    description: "Prioritize what you need to learn. ",
+  },
+  {
+    description: "Freelance (the boring stuff) and career advice",
+  },
+  {
+    description: "Help you setting goals and actually reaching them",
+  },
+]
+
+const get = [
+  {
+    description: "4+ months mentorship plan",
+  },
+  {
+    description: "Bi-weekly  1-on-1 in-person chats",
+  },
+  {
+    description: "Resources curated towards your goals",
+  },
+  {
+    description: "Always availabe for inbetween direct messages",
+  },
+  {
+    description: "Fun assignments and challenges",
+  },
+]
+
+const works = [
+  {
+    description: "Fill out some questions about yourself.",
+  },
+  {
+    description: "A brief write-up (or video) as to why you’d like me to mentor you",
+  },
+  {
+    description: "Number of sessions and payment agreement. The fee is ... euros per 0:00h session.",
+  },
+]
+
+module.exports = {
+  help,
+  get,
+  works
+}
