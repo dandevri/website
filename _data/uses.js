ad1f0f9bbbcb1b1f50ccc99f8f4da0822de85a4f
dandevri <hi@dandevri.es>
2019-08-01 11:23:30 +0200

    Update uses page

--- _data/uses.js
+++ _data/uses.js
@@ -1,25 +1,288 @@
 const workstation = [
   {
-    title: "Macbook 13 inch (Late 2017)",
-    link: "www.apple.com",
-    description: "This is my main machine and what I use for 99% of my work.",
+    title: "MacBook Po 13-inch (Late 2017 with touchbar)",
+    link: "https://www.apple.com/nl/shop/buy-mac/macbook-pro",
+    description: "This is my main machine that I use 90% of the time. I'm not a huge fan of the keyboard and don't use the touchbar very often. The health of my battery seems to decrease very quickly. Overall not my favorite computer but it's sleek, thin and I'm pretty locked in the Apple Ecosystem.",
   },
   {
-    title: "Logitech MX Master",
-    link: "https://www.apple.com/nl/",
-    description: "Mouse that I use to point",
+    title: "Logitech MX Master 2S",
+    link: "https://www.logitech.com/en-us/product/mx-master-2s-flow",
+    description: "Probably the most ergonomic mouse, it fits my hand perfectly. Lots of useful buttons that can be assigned to common action within the logitech software.",
+  },
+  {
+    title: "Dell UltraSharp 25-inch (U2515H) Monitor",
+    link: "https://www.dell.com/ee/business/p/dell-u2515h-monitor/pd",
+    description: "Best bang for your buck. It's the perfect size for a monitor and is well designed. The color calibration is superb and has lots of ports.",
+  },
+  {
+    title: "Satechi Vertical Aluminum Laptop Stand",
+    link: "https://satechi.net/products/universal-vertical-aluminum-laptop-stand",
+    description: "I've played around with multiple monitors and having my MacBook on a stand on the side but figured out I was comfortable using only one screen, so my MacBook stays docked all the time.",
+  },
+  {
+    title: "Magic Keyboard with Numpad (wired)",
+    link: "https://www.apple.com/shop/product/MQ052LL/A/magic-keyboard-with-numeric-keypad-us-english-silver",
+    description: "Still does the job but I'm considering switching (or building) a mechanical keyboard. Still figuring out the right switches and if it should be wireless.",
+  },
+  {
+    title: "Beyerdynamic DT 770 PRO headphone",
+    link: "https://europe.beyerdynamic.com/dt-770-pro.html",
+    description: "Clean and balanced reference headphone for listing to music and watching video's. Not to overpowered bass and a very comfortable fit for long sessions",
   },
 ]
 
-const software = [
+const mobile = [
   {
-    title: "VScode",
-    link: "www.apple.com",
-    description: "Code editor of choice",
+    title: "iPad Pro 13 inch Space Grey",
+    link: "https://www.apple.com/lae/ipad-pro/",
+    description: "An almost laptop replacement for on the go. With the iPad OS update it's almost a full fledge workstation and with the keyboard cover and pencil typing and annotating documents are a breeze.",
+  },
+  {
+    title: "Sony WH-H900N wireless headphones",
+    link: "https://www.sony.com/electronics/headband-headphones/wh-h900n",
+    description: "Wireless and noise cancelling are musts when I'm on the go. Sleek design and a bit bass heavy which I like while I'm listing to music or podcasts while traveling.",
+  },
+  {
+    title: "OnePlus 6 Midnight Black",
+    link: "https://www.oneplus.com/6",
+    description: "Yes, for some reason I like Android as a phone OS. The camera might be a bit lacking in the current age but besides that, I don't feel any reason to upgrade to another phone.",
+  },
+  {
+    title: "Kobo Clara HD eReader",
+    link: "https://us.kobobooks.com/products/kobo-clara-hd?utm_source=Kobo&utm_medium=TopNav&utm_campaign=Clara%20HD",
+    description: "Most of my books I buy in physical form (paperback) but short, brief or e-book only books go on my e-reader.",
+  },
+]
+
+const development = [
+  {
+    title: "Visual Studio Code",
+    link: "https://code.visualstudio.com/",
+    description: "Code editor of choice and use the intergrated terminal along with it. Switched from using Atom and Hyper. Currently using the Night Owl Theme with Victor Mono.",
+  },
+  {
+    title: "Extensions and settings",
+    link: "https://gist.github.com/dandevri/f103267419247a7f92297c6be0a6f529",
+    description: "This is a GitHub gist with a list of all my editor settings, keybindings and extensions in Visual Studio Code.",
+  },
+  {
+    title: "Dotfiles",
+    link: "https://github.com/dandevri/dotfil.es",
+    description: "My configuration files (e.g. configs, linters) are in this GitHub repo. It also contains a list of settings and themes for different Mac Apps.",
+  },
+  {
+    title: "Companion services",
+    link: "https://github.com/dandevri",
+    description: "I keep al my public and private repositories on GitHub. Most of these projects (that are static sites) get deployed trough Netlify. I register and manage all my domains on TransIP.",
+  },
+]
+
+const recording = [
+  {
+    title: "Open Broadcast Software",
+    link: "https://obsproject.com/",
+    description: "Most people see OBS as live streaming software but it's also great to record my screen for courses and tutorials.",
+  },
+  {
+    title: " Devine BM-1000 studio microphone",
+    link: "https://www.bax-shop.nl/grootmembraan-condensatormicrofoons/devine-bm-1000-condensator-studiomicrofoon",
+    description: "Bit of a vintage look and sounds great for the money while being a XLR condensator microphone.",
+  },
+  {
+    title: "Behringer XENYX Q502USB Mixer",
+    link: "https://www.bax-shop.nl/analoog-mengpaneel/behringer-xenyx-q502usb-mixer",
+    description: "Small USB audio interface for connecting my Microphone to my MacBook with USB. It has a built-in equalizer for small voice tweaks.",
+  },
+]
+
+
+const applications = [
+  {
+    title: "1Password",
+    link: "https://1password.com/",
+    description: "Best password manager out there",
+  },
+  {
+    title: "Firefox",
+    link: "https://www.mozilla.org/en-US/firefox",
+    description: "Main browser and devtools of choice",
+  },
+  {
+    title: "Private internet access",
+    link: "https://www.privateinternetaccess.com/",
+    description: "Virtual Private Network Service",
+  },
+  {
+    title: "Stack",
+    link: "https://www.transip.nl/stack/",
+    description: "Online cloud storage",
+  },
+
+  {
+    title: "Spotify",
+    link: "https://www.spotify.com/nl/",
+    description: "Digital Music Service",
+  },
+  {
+    title: "IINA",
+    link: "https://iina.io/",
+    description: "Modern media player for macOS",
+  },
+  {
+    title: "Apple Mail",
+    link: "https://support.apple.com/en-us/HT204093",
+    description: "The trusty default mail app",
+  },
+]
+
+const design = [
+  {
+    title: "UI Design Tools",
+    link: "#",
+    description: "Most of my prototyping happens straight in the browser. I know my way around tools such as Sketch, Invision Studio, Adobe XD, Framer X woring with different clients and teams but personally I'll usually go with Figma.",
+  },
+  {
+    title: "Photo editing",
+    link: "#",
+    description: "I don't do a lot of heavy photo-editing but usually some light touch up work. Mostly in Lightroom CC or Pixelmator.",
+  },
+  {
+    title: "Design Utilities",
+    link: "#",
+    description: "I like small design tools for specific use case. DotGrid and Ronin are some of my favorites.",
+  },
+]
+
+const edc = [
+  {
+    title: "XD Design Bobby Pro Backpack",
+    link: "https://www.xd-design.com/us-us/bobby-pro-anti-theft-backpack-black?returnurl=%2fus-us%2fsearch%3fq%3dpro",
+    description: "I like bags with lots of pockets and dividers, it has little features such as a card holder and key holder. It's water-resistant, looks techy and has a padded laptop compartment.",
+  },
+  {
+    title: "Orbit Key Active Black",
+    link: "https://www.orbitkey.com/collections/key-organiser/products/orbitkey-2-0-active?variant=8198980993120",
+    description: "Key Organiser, keeps my keys neat and tidy and fits perfectly on the key holder of the backpack.",
+  },
+  {
+    title: "Secrid Wallet Slim Vintage Black",
+    link: "https://secrid.com/en-nl/collections/slimwallet/slimwallet-vintage-black",
+    description: "Compact to keep most of my cards and some loose cash. Can't live without the quick access pop-up card holder which is RFID blocking.",
+  },
+  {
+    title: "Casio watches",
+    link: "#",
+    description: "I'm a casio watch collector and have most of their watches from the vintage collection. Sometimes I wear my TicWatch S2 or Pebble Classic when I have lots of meetings to take a quick glance at my calendar.",
+  },
+  {
+    title: "Tech survival Kit",
+    link: "#",
+    description: "Pouch with small physical (hardware) utilities that come in handy ever once in a while such as cables, powerbanks, dongles etc.",
+  },
+]
+
+const utilities = [
+  {
+    title: "Alfred",
+    link: "https://www.alfredapp.com/",
+    description: "MacOS swiss army knife and a great spotlight replacement."
+  },
+  {
+    title: "Spectacle",
+    link: "https://www.spectacleapp.com/",
+    description: "Window management with handy keyboard shortcuts"
+  },
+  {
+    title: "iStat Menus",
+    link: "https://bjango.com/mac/istatmenus/",
+    description: "Mac system monitor but also useful for date and time controls"
+  },
+  {
+    title: "CleanMyMac X",
+    link: "https://macpaw.com/cleanmymac",
+    description: "Removes junk and optimizes your mac"
+  },
+  {
+    title: "HazeOver",
+    link: "https://hazeover.com/",
+    description: "Highlights windows by fading out all others"
+  },
+  {
+    title: "Bartender",
+    link: "https://www.macbartender.com/",
+    description: "Hides mac menu bar items"
+  },
+]
+
+const websites = [
+  {
+    title: "Notion",
+    link: "https://www.notion.so/",
+    description: "For keeping my life together",
+  },
+  {
+    title: "Feedbin",
+    link: "https://feedbin.com/",
+    description: "Simple RSS feed reader",
+  },
+  {
+    title: "Slack",
+    link: "https://slack.com/intl/en-nl/",
+    description: "Avoiding work tool",
+  },
+  {
+    title: "Google calendar",
+    link: "https://calendar.google.com",
+    description: "Trying not to get burned out",
+  },
+]
+
+const home = [
+  {
+    title: "Smart Home Setup",
+    link: "#",
+    description: "I spend quite some time putting tech in my home such as smart-ligting, plugs and voice enabled devices. I've written about all the tech in length here.",
+  },
+  {
+    title: "Video Game setup",
+    link: "#",
+    description: "In my free time I play lots of different games, varying from triple a titles to indie games on different consoles.",
+  },
+]
+
+const workflows = [
+  {
+    title: "Writing workflow",
+    link: "#",
+    description: "A post about my writing workflow",
+  },
+  {
+    title: "Notion Setup",
+    link: "#",
+    description: "A post about my personal notion setup",
+  },
+  {
+    title: "Distractionless Devices",
+    link: "#",
+    description: "A post about managing notifications",
+  },
+  {
+    title: "Recording workflow",
+    link: "#",
+    description: "A post about how podcasts and courses are recorded",
   },
 ]
 
 module.exports = {
   workstation,
-  software
+  development,
+  recording,
+  applications,
+  design,
+  edc,
+  home,
+  mobile,
+  utilities,
+  websites,
+  workflows,
 }
