47de8cb14e91c33c3e72b40566ac22899208bf61
dandevri <hi@dandevri.es>
2019-08-02 11:08:02 +0200

    Update speaking page

---
+++ _data/interviews.js
@@ -0,0 +1,14 @@
+const interviews = [
+  {
+    title: "Golden Dot Awards 2019",
+    link: "https://www.cmd-amsterdam.nl/gda-2019/",
+    date: "2019-07-03",
+    location: "Amsterdam, The Netherlands",
+    video: "youtube.com",
+    slides: "slides.com"
+  },
+]
+
+module.exports = {
+  interviews
+}
