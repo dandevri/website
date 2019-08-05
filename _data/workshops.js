50afb8804259f76e7085a11c06fc7967c58b6a7d
dandevri <hi@dandevri.es>
2019-08-05 11:38:46 +0200

    Add courses and classes

---
+++ _data/workshops.js
@@ -0,0 +1,27 @@
+const workshops = [
+  {
+    title: "Workshop Title",
+    image: "/static/img/talk.jpg",
+    link: "https://cmda-bt.github.io/be-course-18-19/docs/",
+    description: "In Backend we peek behind the curtains and inspect what’s behind the web. You build web apps with Node.js, communicate with HTTP, and store data in a database with MongoDB. In this course you’ll advance your web dev skills. You’ll learn to use computers to actually make what you design work: people can actually fill in forms, like things, and upload files.",
+    location: "University of Applied Sciences Amsterdam",
+  },
+  {
+    title: "Workshop Title",
+    image: "/static/img/talk.jpg",
+    link: "https://cmda-bt.github.io/be-course-18-19/docs/",
+    description: "We kick Blok Tech @CMDA off with a JavaScript bootcamp before we start with Back-end, Front-end 2 and Project Tech. This bootcamp will refresh and build further on the knowledge acquired in Internetstandaarden, Inleiding Programmeren, and Blok Web.",
+    location: "University of Applied Sciences Amsterdam",
+  },
+  {
+    title: "Workshop Title",
+    image: "/static/img/talk.jpg",
+    link: "https://cmda-bt.github.io/be-course-18-19/docs/",
+    description: "The course Systematic UI design teaches students to design User Interfaces in a more modular way. We explore Atomic Design principles and see how we can actually implement those principles using Sketch and Principle.",
+    location: "University of Applied Sciences Amsterdam",
+  },
+]
+
+module.exports = {
+  workshops
+}
