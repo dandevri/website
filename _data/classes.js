f6e3efc56dee7a76fabb387f48b2ccec6282067e
dandevri <hi@dandevri.es>
2019-08-05 13:13:36 +0200

    Update layout

--- _data/classes.js
+++ _data/classes.js
@@ -13,8 +13,16 @@ const classes = [
     description: "We kick Blok Tech @CMDA off with a JavaScript bootcamp before we start with Back-end, Front-end 2 and Project Tech. This bootcamp will refresh and build further on the knowledge acquired in Internetstandaarden, Inleiding Programmeren, and Blok Web.",
     location: "University of Applied Sciences Amsterdam",
   },
+  {
+    title: "Tutorials",
+    image: "/static/img/talk.jpg",
+    link: "https://cmda-bt.github.io/be-course-18-19/docs/",
+    description: "We kick Blok Tech @CMDA off with a JavaScript bootcamp before we start with Back-end, Front-end 2 and Project Tech. This bootcamp will refresh and build further on the knowledge acquired in Internetstandaarden, Inleiding Programmeren, and Blok Web.",
+    location: "University of Applied Sciences Amsterdam",
+  },
 ]
 
 module.exports = {
   classes
 }
+ 
