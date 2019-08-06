b71656e072c3e56a13fd0786f3caeba7c17a7c5a
dandevri <hi@dandevri.es>
2019-08-06 09:55:01 +0200

    Update interview component

--- _data/interviews.js
+++ _data/interviews.js
@@ -1,34 +1,26 @@
 const interviews = [
   {
-    title: "Interview in a certain publication",
-    link: "#",
-    date: "2019-07-03",
+    title: "The Good, The Bad, and The Interesting",
+    episode: "Danny de Vries",
+    link: "https://vasilis.nl/gbi/2017/07/20/danny-de-vries/",
+    publication: "Vasilis van Gemert",
+    date: "2019-20-07",
+    type: "podcast"
   },
   {
-    title: "Interview in a certain publication",
-    link: "#",
-    date: "2019-07-03",
+    title: "Free-riders in a study working group",
+    link: "https://hvana.nl/lees/7358/meelifters-in-je-werkgroep-hoe-herken-je-ze-en-hoe-ga-je-met-ze-om",
+    date: "2018-17-10",
+    publication: "HvanA",
+    type: "article"
   },
   {
-    title: "Interview in a certain publication",
-    link: "#",
-    date: "2019-07-03",
-  },
-  {
-    title: "Interview in a certain publication",
-    link: "#",
-    date: "2019-07-03",
-  },
-  {
-    title: "Interview in a certain publication",
-    link: "#",
-    date: "2019-07-03",
-  },
-  {
-    title: "Interview in a certain publication",
-    link: "#",
-    date: "2019-07-03",
-  },
+    title: "The Babbelbox Podcast",
+    link: "https://thebabbelbox.nl/",
+    date: "2018-17-10",
+    publication: "Rijk van Zanten",
+    type: "podcast"
+  }
 ]
 
 module.exports = {
