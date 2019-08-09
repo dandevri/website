2b2243262629a17a7688d7262d91a997bb2d913b
dandevri <hi@dandevri.es>
2019-08-09 19:57:57 +0200

    Refactor molecule components

--- _data/events.js
+++ _data/events.js
@@ -4,6 +4,28 @@ const events = [
     link: "https://www.cmd-amsterdam.nl/gda-2019/",
     date: "2019-07-03",
     location: "Amsterdam, The Netherlands",
+    appear: "attending",
+    slides: "#"
+  },
+  {
+    title: "Golden Dot Awards 2019",
+    link: "https://www.cmd-amsterdam.nl/gda-2019/",
+    date: "2019-07-03",
+    location: "Amsterdam, The Netherlands",
+    video: "#"
+  },
+  {
+    title: "Golden Dot Awards 2019",
+    link: "https://www.cmd-amsterdam.nl/gda-2019/",
+    date: "2019-07-03",
+    location: "Amsterdam, The Netherlands",
+    appear: "attending"
+  },
+  {
+    title: "Golden Dot Awards 2019",
+    link: "https://www.cmd-amsterdam.nl/gda-2019/",
+    date: "2019-07-03",
+    location: "Amsterdam, The Netherlands",
     appear: "attending"
   },
 ]
