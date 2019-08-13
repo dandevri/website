926dec23c18e5d21baf6f4aa73d4a228f1d61ddf
dandevri <hi@dandevri.es>
2019-08-13 10:55:54 +0200

    Update events

--- _data/events.js
+++ _data/events.js
@@ -5,21 +5,24 @@ const events = [
     date: "2019-07-03",
     location: "Amsterdam, The Netherlands",
     appear: "attending",
-    slides: "#"
+    slides: "#",
+    logo: "/static/img/cmd_icon_bulb.png"
+  },
+  {
+    title: "Fronteers 2019",
+    link: "https://www.cmd-amsterdam.nl/gda-2019/",
+    date: "2019-07-03",
+    location: "Amsterdam, The Netherlands",
+    video: "#",
+    logo: "/static/img/fronteers.png"
   },
   {
     title: "Golden Dot Awards 2019",
     link: "https://www.cmd-amsterdam.nl/gda-2019/",
     date: "2019-07-03",
     location: "Amsterdam, The Netherlands",
-    video: "#"
-  },
-  {
-    title: "Golden Dot Awards 2019",
-    link: "https://www.cmd-amsterdam.nl/gda-2019/",
-    date: "2019-07-03",
-    location: "Amsterdam, The Netherlands",
-    appear: "attending"
+    appear: "attending",
+    logo: "/static/img/cmd_icon_bulb.png"
   },
   {
     title: "Golden Dot Awards 2019",
