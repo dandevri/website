630a5cdaecd8d3a3045740910ebdaf713b9e39c8
dandevri <hi@dandevri.es>
2019-08-13 11:45:02 +0200

    Update layouts

--- _data/classes.js
+++ _data/classes.js
@@ -3,7 +3,7 @@ const classes = [
     title: "Youtube Channel",
     image: "/static/img/talk.jpg",
     link: "https://cmda-bt.github.io/be-course-18-19/docs/",
-    description: "Making Youtube video's as a way to teach and show how I work and think as a UX engineer.",
+    description: "Making Youtube video's as a way to teach and show how I work and think on a daily basis.",
     location: "University of Applied Sciences Amsterdam",
     type: "youtube"
   },
