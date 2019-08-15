123183600f9e52ed5535aaad5871d6598bc559ca
dandevri <hi@dandevri.es>
2019-08-15 10:45:39 +0200

    Layout tweaks

--- _data/workshops.js
+++ _data/workshops.js
@@ -10,6 +10,18 @@ const workshops = [
     image: "/static/img/talk.jpg",
     link: "https://cmda-bt.github.io/be-course-18-19/docs/",
     description: "Making time for what matters. Creating time for your priorities by rethinking the defaults of constant busyness and distraction.",
+  },
+  {
+    title: "Making Time",
+    image: "/static/img/talk.jpg",
+    link: "https://cmda-bt.github.io/be-course-18-19/docs/",
+    description: "Making time for what matters. Creating time for your priorities by rethinking the defaults of constant busyness and distraction.",
+  },
+  {
+    title: "Making Time",
+    image: "/static/img/talk.jpg",
+    link: "https://cmda-bt.github.io/be-course-18-19/docs/",
+    description: "Making time for what matters. Creating time for your priorities by rethinking the defaults of constant busyness and distraction.",
   }
 ]
 
