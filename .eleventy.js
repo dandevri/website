41869674e3f055fcf9cf262717cecf3e44a459f9
dandevri <hi@dandevri.es>
2019-08-22 16:16:01 +0200

    Update speaking resources

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,7 @@ module.exports = function(config) {
   config.addPassthroughCopy("static/img");
   config.addPassthroughCopy("static/fonts");
   config.addPassthroughCopy("static/js");
+  config.addPassthroughCopy("static/other");
   config.addPassthroughCopy("css/style.css");
 
   config.addPlugin(pluginRss);
