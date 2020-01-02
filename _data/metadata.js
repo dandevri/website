c2362a20fb61bad79f472afba8025e92145e6464
dandevri <hi@dandevri.es>
2020-01-02 19:28:35 +0100

    Update head tags

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,12 +2,14 @@ const metadata = {
   title: "Danny de Vries",
   author: "Danny de Vries",
   mail: "hi@dandevri.es",
-  url: "https://example.com",
-  description: "A boilerplate for building static websites",
-  theme: "#222",
+  url: "https://www.dandevri.es",
+  description: "Hi, I'm Danny (@dandevri), a freelance front-end designer and lecturer at the Amsterdam University of Applied Sciences where I teach about code in the technical courses of our program such as front-end and back-end.",
+  subject: "Personal Site of Danny de Vries",
+  theme: "#000",
   lang: "en_US",
-  share: "/static/img/social/",
-  social: "dandevri"
+  share: "/static/img/",
+  social: "dandevri",
+  license: "https://github.com/systemdes/personal-website/blob/master/license"
 }
 
 module.exports = metadata;
