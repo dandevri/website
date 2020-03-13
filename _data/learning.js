2ee4df5df320117b1419acfce256aeb667bca902
dandevri <hi@dandevri.es>
2020-03-13 21:46:29 +0100

    Add learning log

---
+++ _data/learning.js
@@ -0,0 +1,46 @@
+const learning = [
+  {
+    title: "Webflow University Freelance Journey",
+    author: "Webflow",
+    tag: "business",
+    read: "2020",
+    format: "online course",
+    url: "https://university.webflow.com/freelancer"
+  },
+  {
+    title: "Basecamp Shape Up",
+    author: "Ryan Singer",
+    tag: "business",
+    read: "2020",
+    format: "online book",
+    url: "https://basecamp.com/shapeup"
+  },
+  {
+    title: "Design for Developers",
+    author: "Sarah Drasner",
+    tag: "code",
+    read: "2020",
+    format: "course",
+    url: "https://frontendmasters.com/courses/design-for-developers/"
+  },
+  {
+    title: "Getting started with JavaScrip v2",
+    author: "Kyle Simpson",
+    tag: "code",
+    read: "2020",
+    format: "online course",
+    url: "https://frontendmasters.com/courses/getting-started-javascript-v2/"
+  },
+  {
+    title: "Learn modern JavaScript",
+    author: "freeCodeCamp",
+    tag: "code",
+    read: "2020",
+    format: "online course",
+    url: "https://scrimba.com/g/ges6"
+  },
+]
+
+module.exports = {
+ learning
+}
