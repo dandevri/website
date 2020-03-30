7e8ea0d3bf043194a44c5781aa24014811deca0d
dandevri <hi@dandevri.es>
2020-03-30 21:10:06 +0200

    Add blogroll

---
+++ _data/blogroll.js
@@ -0,0 +1,24 @@
+const blogroll = [
+  {
+    name: "Rijk van Zanten",
+    website: "https://rijks.website/",
+    bio: "Rijk is the Lead Developer at Monospace Inc where he builds the Directus Suite. He was my study buddy during the first year at CMD Amsterdam. He since moved to the United States.",
+    avatar: "https://avatars.io/twitter/rijkvanzanten",
+  },
+  {
+    name: "Titus Wormer",
+    website: "https://wooorm.com/",
+    bio: "Titus works full-time on open source maintaining 400+ projects that are downloaded 3B+ times a year. Most of his work is as a core team member of unified. He was one of my former instructors at CMD Amsterdam and one of the main reasons I started teaching at the same program.",
+    avatar: "https://avatars.io/twitter/wooorm",
+  },
+  {
+    name: "Laurens Aarnoudse",
+    website: "https://codepen.io/Razpudding/",
+    bio: "Laurens is a fellow teacher at CMD Amsterdam, developer and a lot of other things. Interested in datavisualisation, ethics, sustainability, and the world in general. He sometimes use the Random Industries brand to take on creative challenges.",
+    avatar: "https://avatars1.githubusercontent.com/u/8708858?s=460&u=81cbb12e681dd4b6bca2f0cdeea4e5ad8a9a6445&v=4",
+  },
+]
+
+module.exports = {
+  blogroll
+}
