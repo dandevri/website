ca300d63bdbaf3bf422a6343a60254bc65a886f5
dandevri <hi@dandevri.es>
2020-04-10 12:56:34 +0200

    Update blogroll

--- _data/blogroll.js
+++ _data/blogroll.js
@@ -17,6 +17,54 @@ const blogroll = [
     bio: "Laurens is a fellow teacher at CMD Amsterdam, developer and a lot of other things. Interested in datavisualisation, ethics, sustainability, and the world in general. He sometimes use the Random Industries brand to take on creative challenges.",
     avatar: "https://avatars1.githubusercontent.com/u/8708858?s=460&u=81cbb12e681dd4b6bca2f0cdeea4e5ad8a9a6445&v=4",
   },
+  {
+    name: "Daan Rongen",
+    website: "https://daan.website/",
+    bio: "Daan uses computational power to enhance everyday objects, places, and experiences working at data visualization agency CLEVERºFRANKE. Fellow student and later fellow freelance teacher for a short while.",
+    avatar: "https://avatars.io/twitter/daanrongen",
+  },
+  {
+    name: "Ioannis (Janno) Kapritsias",
+    website: "https://www.softsquare.nl/",
+    bio: "Janno is a software developer at is own company Softsquare and fellow teacher at CMD Amsterdam teaching Front-end and Introduction to Programming.",
+    avatar: "https://avatars.io/twitter/ioannisDevelop",
+  },
+  {
+    name: "Daniel Shiffman",
+    website: "https://shiffman.net/",
+    bio: "Daniel Shiffman works as an Associate Arts Professor at the Interactive Telecommunications Program at NYU’s Tisch School of the Arts. But he's probably better known for his Youtube channel 'The Coding Train', a great inspiration for my own teaching..",
+    avatar: "https://avatars.io/twitter/shiffman",
+  },
+  {
+    name: "Derek Sivers",
+    website: "https://sivers.org/",
+    bio: "Derek Sivers is an American writer, musician, programmer and entrepreneur. His blog is probably the most thought-provoking blog I follow.",
+    avatar: "https://avatars.io/twitter/sivers",
+  },
+  {
+    name: "Max Böck",
+    website: "https://mxb.dev/",
+    bio: "Max Böck is a front-end web developer and designer based in Vienna, Austria. He's been building stuff on the web for the last 12 years, working with clients across various countries and industries. He was the first to introduce me to the Indieweb concept and the whole reason I started working on my personal site.",
+    avatar: "https://avatars.io/twitter/mxbck",
+  },
+  {
+    name: "Mike Monteiro",
+    website: "https://muledesign.com/",
+    bio: "Mike Monteiro is the co-founder and design director of Mule Design. He prefers that designers have strong spines. Mike writes and speaks frequently about the craft, ethics, and business of design. He's the one that made me realize design ethics was a thing.",
+    avatar: "https://avatars.io/twitter/monteiro",
+  },
+  {
+    name: "Cassie Evans",
+    website: "https://www.cassie.codes/",
+    bio: "Cassie makes things on the web. During the week she makes accessible, progressively enhanced things for Clearleft and in her spare fun, animated things on Codepen. She brings the fun back into development.",
+    avatar: "https://avatars.io/twitter/cassiecodes",
+  },
+  {
+    name: "Cassidy Williams",
+    website: "https://cassidoo.co/",
+    bio: "Cassidy is an instructor and senior software engineer at React Training. Before now, she worked at CodePen, Amazon, L4 Digital, Clarifai, and Venmo, and graduated from Iowa State University. Her TikTok style video's on Twitter are the best.",
+    avatar: "https://avatars.io/twitter/cassidoo",
+  },
 ]
 
 module.exports = {
