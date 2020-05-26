e1ce21142161774ba407998f80fa782aab73e5d8
dandevri <hi@dandevri.es>
2020-05-26 20:04:29 +0200

    Tweak vertical spacing

--- _data/events.js
+++ _data/events.js
@@ -7,6 +7,24 @@ const events = [
     appear: "attending",
     slides: "#",
     logo: "/static/img/cmd_icon_bulb.png"
+  },
+  {
+    title: "Golden Dot Awards 2019",
+    link: "https://www.cmd-amsterdam.nl/gda-2019/",
+    date: "2019-07-03",
+    location: "Amsterdam, The Netherlands",
+    appear: "attending",
+    slides: "#",
+    logo: "/static/img/cmd_icon_bulb.png"
+  },
+  {
+    title: "Golden Dot Awards 2019",
+    link: "https://www.cmd-amsterdam.nl/gda-2019/",
+    date: "2019-07-03",
+    location: "Amsterdam, The Netherlands",
+    appear: "attending",
+    slides: "#",
+    logo: "/static/img/cmd_icon_bulb.png"
   }
 ]
 
