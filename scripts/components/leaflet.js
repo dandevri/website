dbe0126db18096f47099b1cfdc4f81070fd640ab
dandevri <hi@dandevri.es>
2021-02-07 20:48:23 +0100

    Update overview styling

--- scripts/components/leaflet.js
+++ scripts/components/leaflet.js
@@ -2,7 +2,7 @@
   Show my events plotted on a worldmap with markers.
 */
 
-const mymap = L.map('mapid').setView([0, 0], 0).setZoom(2);
+const mymap = L.map('mapid').setView([40, 30], 0).setZoom(3);
 
 // Create a custom marker
 const icon = L.icon({
@@ -14,16 +14,13 @@ const icon = L.icon({
 document.getElementsByClassName( 'leaflet-control-attribution' )[0].style.display = 'none';
 
 // Tile the worldmap from mapbox
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/dandevri/ckkvjrjop0q5r17qqitvmhp7q/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGFuZGV2cmkiLCJhIjoiY2tmbGN3cmgzMGlxNzJ0bmRudnMwODd5dCJ9.pJY0KKw84BmvNeTyY3A-kw', {
     maxZoom: 18,
-    id: 'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZGFuZGV2cmkiLCJhIjoiY2tmbGN3cmgzMGlxNzJ0bmRudnMwODd5dCJ9.pJY0KKw84BmvNeTyY3A-kw'
 }).addTo(mymap);
 
 // Create markers and bind pop-ups
-const marker = L.marker([51.5, -0.09], {icon: icon}).addTo(mymap);
-const marker2 = L.marker([60, -0.10], {icon: icon}).addTo(mymap);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
-marker2.bindPopup("<b>Hello world!</b><br>I am a popup.");
+const marker = L.marker([52.35938447998207, 4.908699214377195], {icon: icon}).addTo(mymap);
+marker.bindPopup("<b>Golden Dot Awards</b><br>Amsterdam, The Netherlands").openPopup();
